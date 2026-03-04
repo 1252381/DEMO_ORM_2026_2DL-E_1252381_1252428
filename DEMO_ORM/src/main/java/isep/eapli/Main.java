2763e1a269a892b78dacb33ee27f41b7e150a0ee
BartekS5 <seczkowskibartek@gmail.com>
2026-03-04 16:31:12 +0000

    Created the Maven project with the skeleton of the main class

--- DEMO_ORM/src/main/java/isep/eapli/Main.java
+++ DEMO_ORM/src/main/java/isep/eapli/Main.java
@@ -1,4 +1,4 @@
-package isep.eapli.demo_orm;
+package isep.eapli;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
